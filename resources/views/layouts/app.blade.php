01a9d6da062c7b6e05f08388a83d6890441358a0
setyaagung <setyaagung74@gmail.com>
2020-08-04 18:46:28 +0700

    memperbaiki gambar produk yg belum muncul

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>@yield('title')</title>
 
